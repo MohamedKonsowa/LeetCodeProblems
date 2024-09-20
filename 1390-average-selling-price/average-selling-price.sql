a03d516b576649a1784b9b418bebb2b454e21a08
Mohamed Konsowa <79362967+MohamedKonsowa@users.noreply.github.com>
2024-09-20 05:59:15 +0300

    Time: 313 ms (83.79%) | Memory: 0B (100.00%) - LeetSync

--- 1390-average-selling-price/average-selling-price.sql
+++ 1390-average-selling-price/average-selling-price.sql
@@ -1,10 +1,7 @@
 /* Write your T-SQL query statement below */
 SELECT 
     p.product_id,
-    CASE
-        WHEN SUM(u.units) IS NULL OR SUM(u.units) = 0 THEN 0
-        ELSE ROUND((1.0 * SUM(p.price * u.units)) / (1.0 * SUM(u.units)), 2)
-    END AS average_price
+    ISNULL(ROUND((1.0 * SUM(p.price * u.units)) / (1.0 * SUM(u.units)), 2),0) as average_price
 FROM Prices p
 LEFT JOIN UnitsSold u 
     ON p.product_id = u.product_id 
