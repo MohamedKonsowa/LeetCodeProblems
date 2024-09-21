dfe308133524e0b99929101f2b958af3606fab4d
Mohamed Konsowa <79362967+MohamedKonsowa@users.noreply.github.com>
2024-09-21 06:40:32 +0300

    Time: 288 ms (95.61%) | Memory: 94.1 MB (69.27%) - LeetSync

--- 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.cs
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.cs
@@ -2,17 +2,10 @@ public class Solution
 {
     public int[] ReplaceElements(int[] arr)
     {
-        if (arr.Length == 1)
-        {
-            arr[0] = -1;
-            return arr;
-        }
-
-        int maxRight = Math.Max(arr[^1], arr[^2]);
+        int maxRight = arr[^1];
         int temp;
-        arr[^2] = arr[^1];
         arr[^1] = -1;
-        for (int i = arr.Length - 3; i >= 0; i--)
+        for (int i = arr.Length - 2; i >= 0; i--)
         {
             temp = arr[i];
             arr[i] = maxRight;
