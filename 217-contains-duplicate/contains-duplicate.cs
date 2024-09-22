f2ee06ef592f609f614d58297082edac860e6bf4
Mohamed Konsowa <79362967+MohamedKonsowa@users.noreply.github.com>
2024-09-22 09:07:48 +0300

    Time: 301 ms (12.00%) | Memory: 79.6 MB (5.48%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cs
+++ 217-contains-duplicate/contains-duplicate.cs
@@ -1,14 +1,4 @@
 public class Solution {
-    public bool ContainsDuplicate(int[] nums) 
-    {
-        Dictionary<int ,int>dict = new Dictionary<int,int>();
-        foreach(var n in nums)
-        {
-            if(dict.ContainsKey(n)) return true;
-
-            dict[n] =1;
-        }
-
-        return false;
-    }
+      public bool ContainsDuplicate(int[] nums) 
+        => nums.GroupBy(n => n).Any(g => g.Count() > 1);
 }
