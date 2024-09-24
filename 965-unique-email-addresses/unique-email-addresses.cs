2d15adb3201cf3a186c22cc60d9dda97daa91b3a
Mohamed Konsowa <79362967+MohamedKonsowa@users.noreply.github.com>
2024-09-24 14:32:58 +0300

    Time: 77 ms (23.71%) | Memory: 48.6 MB (32.33%) - LeetSync

--- 965-unique-email-addresses/unique-email-addresses.cs
+++ 965-unique-email-addresses/unique-email-addresses.cs
@@ -4,28 +4,11 @@ public class Solution
     {
         HashSet<string> uniqueEmails = [];
         StringBuilder sb;
-        int i = 0;
         foreach (string email in emails)
         {
             string[]? strings = email.Split('@');
-            sb = new(strings[0]);
-            i = 0;
-            while (i < sb.Length)
-            {
-                if (sb[i] == '+')
-                {
-                     sb  = new(sb.ToString()[..i]);
+            sb = new(strings[0].Split('+')[0].Replace(".",""));
 
-                    break;
-                }
-                else if (sb[i] == '.')
-                {
-                    sb.Remove(i, 1);
-                    continue;
-                }
-
-                i++;
-            }
             sb.Append('@');
             sb.Append(strings[1]);
             uniqueEmails.Add(sb.ToString());
