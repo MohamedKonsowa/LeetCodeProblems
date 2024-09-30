88bde6b057925a342c9cd447a7fd4d473ac01af8
Mohamed Konsowa <79362967+MohamedKonsowa@users.noreply.github.com>
2024-09-30 14:48:01 +0300

    Time: 146 ms (76.32%) | Memory: 80.3 MB (60.93%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.cs
@@ -0,0 +1,37 @@
+public class Solution
+{
+    public IList<IList<string>> GroupAnagrams(string[] strs)
+    {
+        Dictionary<string, List<string>> dict = [];
+        string sortedString;
+        char[] chars = [];
+        List<IList<string>> result = [];
+
+        List<string> list = [];
+        foreach (string str in strs)
+        {
+            chars = str.ToCharArray();
+            Array.Sort(chars);
+            sortedString = new string(chars);
+            if (dict.ContainsKey(sortedString))
+            {
+                dict[sortedString].Add(str);
+            }
+            else
+            {
+                dict.Add(sortedString, [str]);
+            }
+        }
+
+        foreach (var item in dict)
+        {
+            list.AddRange(item.Value);
+
+            result.Add(list);
+
+            list = [];
+        }
+
+        return result;
+    }
+}
