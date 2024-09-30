55d5d6dba87bed50e6cc89d39f219129944a930f
Mohamed Konsowa <79362967+MohamedKonsowa@users.noreply.github.com>
2024-09-30 14:31:12 +0300

    Time: 78 ms (20.35%) | Memory: 41.3 MB (87.45%) - LeetSync

--- 1950-sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.cs
+++ 1950-sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.cs
@@ -2,8 +2,20 @@ public class Solution
 {
     public int ArraySign(int[] nums)
     {
-        if(nums.Any(n=>n==0)) return 0;
-        else if(nums.Count(n=>n<0)% 2 == 0) return 1;
-        else return -1;
+        int negativeCount = 0;
+
+        foreach (int num in nums)
+        {
+            if (num == 0)
+            {
+                return 0;
+            }
+            else if (num < 0)
+            {
+                negativeCount++;
+            }
+        }
+
+        return negativeCount % 2 == 0 ? 1 : -1;
     }
 }
